27c3eeabf8fd14a01c389987ad18534d86f37ae1
“FirstofHerName” <mbe773@gmail.com>
2017-08-28 21:41:09 -0500

    contact

---
+++ db/contact.rb
@@ -0,0 +1,15 @@
+contact= Contact.new(
+                     first_name:"Marcos",
+                     last_name:"Torres", 
+                     email:"matorres1121@gmail.com",
+                     phone_number:"773-418-1721")
+
+contact.save
+
+contact = Contact.new(first_name:"Michelea",
+                     last_name:"Estrada", 
+                     email:"shay324@gmail.com",
+                     phone_number:"773-573-7098"
+                      )
+
+contact.save
---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+contact = Contact.new(
+                     first_name:"Marcos",
+                     last_name:"Torres", 
+                     email:"matorres1121@gmail.com",
+                     phone_number:"773-555-1721")
+
+contact.save
+
+contact = Contact.new(                  
+                     first_name:"Michelea",
+                     last_name:"Estrada", 
+                     email:"shay324@gmail.com",
+                     phone_number:"773-555-7098"
+                      )
+
+contact.save
+
+
+contact = Contact.new(
+                      first_name: "Elia", 
+                      last_name: "Gonzalez", 
+                      email: "mbe773@gmail.com", 
+                      phone_number: "773-555-0235"
+                      )
+
+contact.save
